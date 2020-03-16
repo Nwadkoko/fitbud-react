5fecfc515f50234e9bc0aacb561f6bbec8a1bb52
Matthieu <matthieubeaussart8@gmail.com>
2020-03-16 18:21:58 +0100

    Meal saved to database

--- src/components/nutrition/nutrition.jsx
+++ src/components/nutrition/nutrition.jsx
@@ -1,14 +1,31 @@
 import React from "react";
 
+import firebaseApp from "firebase";
 
 export class Nutrition extends React.Component {
   constructor(props) {
     super(props);
+    this.state = { meal: '' };
+  }
+
+  myChangeHandler = (event) => {
+      this.setState({ meal: event.target.value });
   }
 
   render() {
     return (
-        <div className="nutrition"></div>
+      <div className="nutrition">
+        <form onSubmit={this.myChangeHandler}>
+            <input type="text" onChange={this.myChangeHandler}/>
+            <input type="submit" value="Submit!" onClick={this.submitData} />
+        </form>
+      </div>
     );
+  }
+
+  submitData = (event) => {
+      event.preventDefault();
+      console.log(this.state.meal);
+      firebaseApp.database().ref('/meal').set(this.state.meal);
   }
 }
