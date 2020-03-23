3a3c95a430d53763763b357ebadb55c3949f9ed8
Matthieu <matthieubeaussart8@gmail.com>
2020-03-23 14:10:45 +0100

    Items can be registered to database

---
+++ src/components/nutrition/itemMealModalForm.jsx
@@ -0,0 +1,112 @@
+import React from "react";
+import firebaseApp from "firebase";
+import { Button } from "react-bootstrap";
+import { Form } from "react-bootstrap";
+
+export class ItemMealModalForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      item: "",
+      glucides: 0,
+      lipides: 0,
+      proteines: 0
+    };
+  }
+
+  itemChangeHandler = event => {
+    this.setState({ item: event.target.value });
+  }
+
+  glucidesChangeHandler = event => {
+    this.setState({ glucides: event.target.value }, () =>
+      console.log(this.state.glucides)
+    );
+  }
+
+  lipidesChangeHandler = event => {
+    this.setState({ lipides: event.target.value }, () =>
+      console.log(this.state.lipides)
+    );
+  }
+
+  proteinesChangeHandler = event => {
+    this.setState({ proteines: event.target.value }, () =>
+      console.log(this.state.proteines)
+    );
+  }
+
+  render() {
+    return (
+      <div className="item-form-container">
+        <Form>
+          <Form.Group controlId="formBasicMealName">
+            <Form.Label>Item name</Form.Label>
+            <Form.Control type="meal" placeholder="Enter item name" onChange={this.itemChangeHandler}/>
+          </Form.Group>
+          <Form.Group controlId="formBasicItemGlucides">
+            <Form.Label>Glucides</Form.Label>
+            <Form.Control type="number" placeholder="Enter glucides" onChange={this.glucidesChangeHandler}/>
+          </Form.Group>
+          <Form.Group controlId="formBasicItemLipides">
+            <Form.Label>Lipides</Form.Label>
+            <Form.Control type="number" placeholder="Enter lipides" onChange={this.lipidesChangeHandler}/>
+          </Form.Group>
+          <Form.Group controlId="formBasicItemProteines">
+            <Form.Label>Protéines</Form.Label>
+            <Form.Control type="number" placeholder="Enter proteines" onChange={this.proteinesChangeHandler}/>
+          </Form.Group>
+          <Button variant="secondary">
+            - Remove item
+          </Button>
+          <Button variant="success" onClick={this.submitData}>
+            * Save item
+          </Button>
+        </Form>
+      </div>
+    );
+  }
+
+  submitData = event => {
+    event.preventDefault();
+    console.log(this.state.meal);
+    console.log("signed in");
+
+    var uid = firebaseApp.auth().currentUser.uid;
+    var today = new Date();
+    var date =
+      today.getFullYear() +
+      "-" +
+      (today.getMonth() + 1) +
+      "-" +
+      today.getDate();
+    var time =
+      today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+
+    /*firebaseApp
+      .database()
+      .ref(uid + "/meals/" + date + "/" + time + "/name")
+      .set(this.state.meal);
+    firebaseApp
+      .database()
+      .ref(uid + "/meals/" + date + "/" + time + "/calories")
+      .set(this.state.calories);*/
+
+      firebaseApp
+      .database()
+      .ref(uid + "/items/" + this.state.item + "/glucides/")
+      .set(this.state.glucides);
+
+      firebaseApp
+      .database()
+      .ref(uid + "/items/" + this.state.item + "/lipides/")
+      .set(this.state.lipides);
+
+      firebaseApp
+      .database()
+      .ref(uid + "/items/" + this.state.item + "/proteines/")
+      .set(this.state.proteines);
+  };
+}
+
+export default ItemMealModalForm;
--- src/components/nutrition/mealModalForm.jsx
+++ src/components/nutrition/mealModalForm.jsx
@@ -2,14 +2,32 @@ import React from "react";
 import firebaseApp from "firebase";
 import { Button } from "react-bootstrap";
 import { Form } from "react-bootstrap";
+import ItemMealModalForm from "./itemMealModalForm";
 
 export class MealModalForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-        meal: "",
-        calories: 0
+        count: 1,
+      meal: "",
+      calories: 0
     };
+  }
+
+  addFormItem(){
+      this.setState({count: this.state.count + 1});
+  }
+
+  displainFormItem(){
+      let forms = [];
+      for(let i = 0; i < this.state.count; i++){
+          forms.push(
+              <div key={i}>
+                  <ItemMealModalForm />
+              </div>
+          )
+      }
+      return forms;
   }
 
   mealChangeHandler = event => {
@@ -17,7 +35,9 @@ export class MealModalForm extends React.Component {
   };
 
   caloriesChangeHandler = event => {
-    this.setState({ calories: event.target.value }, () => console.log(this.state.calories));
+    this.setState({ calories: event.target.value }, () =>
+      console.log(this.state.calories)
+    );
   };
 
   /*
@@ -34,11 +54,13 @@ export class MealModalForm extends React.Component {
 
   render() {
     return (
-        <Form>
+      <Form>
         <Form.Group controlId="formBasicMealName">
           <Form.Label>Meal name</Form.Label>
           <Form.Control type="meal" placeholder="Enter meal name" />
         </Form.Group>
+        {this.displainFormItem()}
+        <Button variant="success" onClick={this.addFormItem.bind(this)}>+ Add item</Button>
       </Form>
     );
   }
