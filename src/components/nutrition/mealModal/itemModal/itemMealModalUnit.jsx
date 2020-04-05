c6d49b88ee49b04023c676d6b1903718ab9ec50d
Matthieu <matthieubeaussart8@gmail.com>
2020-04-05 16:31:23 +0200

    Re-organized folders

--- src/components/nutrition/mealModal/itemModal/itemMealModalUnit.jsx
+++ src/components/nutrition/mealModal/itemModal/itemMealModalUnit.jsx
@@ -26,26 +26,13 @@ export class ItemMealModalUnit extends React.Component {
     })
   };
 
+  componentDidMount() {
+      console.log("mounted")
+  }
+
   chooseChangeHandler = (event) => {
     console.log(event.target.value)
     console.log(Object.entries(this.state.items[0][event.target.value]))
-  }
-
-  addItemToMeal() {
-    if(this.state.items.length > 0) {
-      var items = Object.keys(this.state.items[0]).map(item => (
-        <option key={item}>{item}</option>
-      ))
-    }
-
-    return(
-      <Form.Group controlId="formGridState">
-          <Form.Label>Items</Form.Label>
-          <Form.Control as="select" onChange={this.chooseChangeHandler}>
-            {items}
-          </Form.Control>
-      </Form.Group>
-    )
   }
   
   render() {
@@ -66,12 +53,10 @@ export class ItemMealModalUnit extends React.Component {
       <div className="item-form-container">
         <Form.Group controlId="formGridState">
           <Form.Label>Items</Form.Label>
-          <Form.Control as="select" onChange={this.chooseChangeHandler}>
+          <Form.Control as="select" onClick={this.retrieveItems} onChange={this.chooseChangeHandler}>
             {items}
           </Form.Control>
         </Form.Group>
-        <Button onClick={this.retrieveItems} className="button-items">Retrieve</Button>
-        <Button onClick={this.addItemToMeal}>Add item to meal</Button>
       </div>
     );
   }
